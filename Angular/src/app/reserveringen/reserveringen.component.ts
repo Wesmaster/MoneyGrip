3f21cc362654bc88dda13f91943b79a713f4bd8f
Wesley Jonker <wesley.jonker@topicus.nl>
2019-03-24 17:21:52 +0100

    Budgetten, contracten, reserveringen en spaardoelen via shared module.

--- Angular/src/app/reserveringen/reserveringen.component.ts
+++ Angular/src/app/reserveringen/reserveringen.component.ts
@@ -3,72 +3,45 @@ import { MatDialog} from '@angular/material';
 import { DialogBevestigenComponent } from '../dialog-bevestigen/dialog-bevestigen.component';
 import { Reservering } from './reservering/reservering';
 import { ReserveringComponent } from './reservering/reservering.component';
-import { ReserveringService } from './reservering.service';
 import { Maanden } from '../maanden.enum';
 import { CurrencyPipe } from '../currency.pipe';
+import BasisOverzichtComponent  from '../base/basis-overzicht.component';
+import { BasisService } from '../base/basis.service';
 
 @Component({
   selector: 'app-reserveringen',
-  templateUrl: './reserveringen.component.html',
+  templateUrl: '../base/basis-overzicht.component.html',
   styleUrls: ['./reserveringen.component.scss']
 })
-export class ReserveringenComponent implements OnInit {
+export class ReserveringenComponent extends BasisOverzichtComponent implements OnInit {
 
   items: Reservering[];
   MaandenEnum: typeof Maanden = Maanden;
   selectedId: number;
   rowSelected: boolean;
   buttonText = "Reservering";
-  searchText: string;
+  zoekResultaat: Reservering[];
+  titel = "Reserveringen";
+  docpage = this.titel.toLowerCase();
+  tabel: any[];
 
-  constructor(private service: ReserveringService, public dialog: MatDialog, private customCurrency: CurrencyPipe)
+  constructor(public service: BasisService, public dialog: MatDialog, private customCurrency: CurrencyPipe)
   {
+    super(service);
+    service.setAccessPointUrl('reservering');
 
-  }
-
-  ngOnInit()
-  {
-    this.get();
-    this.selectedId = null;
-    this.rowSelected = false;
+    this.tabel = [
+      {kolomnaam: "Categorie", kolombreedte: 2},
+      {kolomnaam: "Label", kolombreedte: 2},
+      {kolomnaam: "Bedrag", kolombreedte: 1},
+      {kolomnaam: "Maand", kolombreedte: 1},
+      {kolomnaam: "Omschrijving", kolombreedte: 0}
+    ];
   }
 
   get(): void
   {
-    this.service.getAll().subscribe(items => this.items = items);
-  }
-
-  onSelect(item: Reservering): void
-  {
-    this.selectedId = item.id;
-    this.rowSelected = true;
-
-    this.openAddDialog(item.id);
-  }
-
-  afterEdit(id): void
-  {
-    if(id !== null)
-    {
-      this.get();
-    }
-    this.selectedId = null;
-    this.rowSelected = false;
-  }
-
-  add(): void
-  {
-    this.selectedId = 0;
-    this.rowSelected = true;
-
-    this.openAddDialog(this.selectedId);
-  }
-
-  verwijderen(id): void
-  {
-    this.service.delete(id).subscribe(item => {
-      this.afterEdit(id);
-    });
+    this.service.getAll().subscribe(items => {this.zoekResultaat = items.map(x => Object.assign(new Reservering(this.customCurrency), x)); this.items = items.map(x => Object.assign(new Reservering(this.customCurrency), x))});
   }
 
   openDeleteDialog(item: Reservering): void
@@ -94,7 +67,7 @@ export class ReserveringenComponent implements OnInit {
     });
   }
 
-  openAddDialog(id): void
+  openAddDialog(id: number): void
   {
     const dialogRef = this.dialog.open(ReserveringComponent, {
       data: id,
@@ -112,5 +85,15 @@ export class ReserveringenComponent implements OnInit {
         this.afterEdit(null);
       }
     });
+  }
+
+  zoek(zoekTekst: string): void
+  {
+    this.zoekResultaat = this.items.filter(
+      item => new RegExp(zoekTekst, 'gi').test(item.labelNavigation.naam) 
+      || new RegExp(zoekTekst, 'gi').test(item.labelNavigation.categorieNavigation.naam)
+      || new RegExp(zoekTekst, 'gi').test(Maanden[item.maand])
+      || new RegExp(zoekTekst, 'gi').test(item.omschrijving)
+    );
   }
 }
